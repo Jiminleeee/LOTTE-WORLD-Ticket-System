b64be328585de373d96d0ecabce5d0850379f948
Jiminleeee <jiminab94@naver.com>
2022-03-24 10:00:11 +0900

    day

--- code/sales analysis.cpp
+++ code/sales analysis.cpp
@@ -5,8 +5,8 @@ int main()
 	const int MAX = 100;
 	int count = 0;
 	int year[MAX], month[MAX], date[MAX], ticketChoiceArr[MAX], ticketTimeArr[MAX], ageArr[MAX], ticketAmountArr[MAX], totalFeeArr[MAX], specialOfferArr[MAX]={0};	
-	int ticketTimeList[MAX]={0}, age1DayList[MAX]={0}, ageAfter4List[MAX]={0}, totalFeeList[MAX]={0}, specialOfferList[MAX]={0}, dateSaleList[MAX]={0}, totalFeeDayList[MAX]={0};
-
+	int ticketTimeList[MAX]={0}, age1DayList[MAX]={0}, ageAfter4List[MAX]={0}, totalFeeList[MAX]={0}, specialOfferList[MAX]={0}, dateSaleList[MAX]={0}, totalFeeDayList[MAX][MAX]={0};
+	int day,datePosition;
 
 	while(fscanf (fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d", &year[count], &month[count], &date[count], &ticketChoiceArr[count], &ticketTimeArr[count], 
 													&ageArr[count], &ticketAmountArr[count], &totalFeeArr[count], &specialOfferArr[count]) != -1)
@@ -26,6 +26,9 @@ int main()
 
 //권종별 판매 현황
 	printf("\n===============권종별 판매 현황=============================\n");
+	
+	
+	
 	for(int index = 0; index < count; index++)
 	{ //1Day 일때  
 		if(ticketTimeArr[index] == 1)
@@ -91,6 +94,18 @@ int main()
 	printf("베이비 %d매, 어린이 %d매, 청소년 %d매, 어른 %d매, 노인 %d매\n", ageAfter4List[0], ageAfter4List[1], ageAfter4List[2], ageAfter4List[3], ageAfter4List[4]); 
 	printf("After4 매출 : %d원\n", totalFeeList[1]);
 	printf("-------------------------------------------------------------------\n\n\n");
+//권종별 매출 파일 출력 
+	FILE * filePointer1 = fopen("Ticketdata.csv", "w");
+	fprintf(fp, "구분,주간권,야간권\n");
+	fprintf(fp, "베이비, %d,%d\n", age1DayList[0], ageAfter4List[0]);
+	fprintf(fp, "어린이, %d,%d\n", age1DayList[1], ageAfter4List[1]);
+	fprintf(fp, "청소년, %d,%d\n", age1DayList[2], ageAfter4List[2]);
+	fprintf(fp, "어른, %d,%d\n", age1DayList[3], ageAfter4List[3]);
+	fprintf(fp, "노인, %d,%d\n", age1DayList[4], ageAfter4List[4]);
+	fprintf(fp, "합계, %d,%d\n", ticketTimeList[0], ticketTimeList[1]);
+	fprintf(fp, "매출, %d,%d\n", totalFeeList[0], totalFeeList[1]);
+	
+	fclose(filePointer1);
 
 /* 일자별 매출 현황
 	printf("=============일자별 매출 현황 ===============\n");
@@ -102,7 +117,7 @@ int main()
 	 	}
 		printf("%d년 %d월 %d일 : 총 매출 %d원\n", year[index], month[index], date[index], totalFeeDayList[index]);
 	}
-*/	
+	
 	for(int index = 0; index <= count; index++)
 	{
 	
@@ -120,8 +135,37 @@ int main()
 			printf("%d\n", totalFeeDayList[index]);
 		
 	}		
-		
+*/	
+	for(int index = 0; index < count; index++)
+	{
 	
+		day = date[index];
+		if(index == 0)
+		{
+			totalFeeDayList[datePosition][0] = day;
+			totalFeeDayList[datePosition][1] += totalFeeArr[index];
+		}
+		else if(date[index] == date[index-1])	
+		{
+			totalFeeDayList[datePosition][1] += totalFeeArr[index];
+		}
+		else
+		{
+			datePosition++;
+			totalFeeDayList[datePosition][0] = day;
+			totalFeeDayList[datePosition][1] += totalFeeArr[index];
+		}
+		if(date[index] == date[index+1])
+		{
+			continue;
+		}
+		else
+		{
+		
+			printf("%d년 %d월 %d일 : 총 매출  %d원\n", year[index], month[index], date[index], totalFeeDayList[datePosition][1]);		
+		}
+	}	
+
 	printf("--------------------------------------------------------\n\n\n");
 	
 
@@ -166,5 +210,16 @@ int main()
 	printf("다둥이 행복카드 	:	%d매\n",specialOfferList[6]);
 	printf("------------------------------------------------\n\n\n");
 
+//우대권 판매 현황 파일 출력
+	FILE * filePointer3 = fopen("special offer.csv", "w");
+	fprintf(fp, "구분, 우대권\n");
+	fprintf(fp,"우대없음, %d\n", specialOfferList[1]); 
+	fprintf(fp,"장애인, %d\n", specialOfferList[2]);
+	fprintf(fp,"국가유공자, %d\n", specialOfferList[3]);
+	fprintf(fp,"휴가 장병, %d\n", specialOfferList[4]);
+	fprintf(fp,"임산부, %d\n", specialOfferList[5]);
+	fprintf(fp,"다둥이 행복카드, %d\n", specialOfferList[6]);     
+	fprintf(fp,"합계, %d\n", specialOfferList[0]); 
+	fclose(filePointer3);
 	return 0;
 } 
